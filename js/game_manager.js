de0c7ed6d2f17c07275ac27202ace17499d6b9b1
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-11-02 17:33:26 -0600

    Change spawnings

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 81 : 243;
+    var value = Math.random() < 0.9 ? 2 : 6;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -153,8 +153,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 729 tile
-          if (merged.value === 729) self.won = true;
+          // The mighty 486 tile
+          if (merged.value === 486) self.won = true;
         }
         
 	 else if (!tile.merged){
